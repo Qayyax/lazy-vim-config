661b52ebda995ce7c49177b0de223bc6be05f9ea
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-05-26 22:49:55 -0400

    fixed none-ls to autoformat with lsp

--- init.lua
+++ init.lua
@@ -622,14 +622,32 @@ require('mason-lspconfig').setup({
 
 -- Setup null-ls
 local null_ls = require("null-ls")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
   sources = {
-    null_ls.builtins.formatting.prettier.with({
-      filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
-    }),
+    null_ls.builtins.formatting.prettier, -- for JS/TS
+    null_ls.builtins.formatting.black,    -- for Python (optional)
+    -- null_ls.builtins.formatting.goimports, -- for Go (optional)
   },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({
+            bufnr = bufnr,
+            filter = function(c)
+              return c.name == "null-ls"
+            end,
+          })
+        end,
+      })
+    end
+  end,
 })
-
 -- Setup neovim lua configuration
 require('neodev').setup()
 
