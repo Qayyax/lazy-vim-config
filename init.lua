d3a19dbe198544396450492f3714eab91eeccb8e
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-05-27 23:56:13 -0400

    fixing cmp to use tab, and updating it

--- init.lua
+++ init.lua
@@ -455,52 +455,54 @@ require("lazy").setup({
 				--
 				-- No, but seriously. Please read `:help ins-completion`, it is really good!
 				mapping = cmp.mapping.preset.insert({
-					-- Select the [n]ext item
 					["<C-n>"] = cmp.mapping.select_next_item(),
-					-- Select the [p]revious item
 					["<C-p>"] = cmp.mapping.select_prev_item(),
-
-					-- Scroll the documentation window [b]ack / [f]orward
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-d>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
-
-					-- Accept ([y]es) the completion.
-					--  This will auto-import if your LSP supports it.
-					--  This will expand snippets if the LSP sent a snippet.
-					["<C-y>"] = cmp.mapping.confirm({ select = true }),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-
-					-- Manually trigger a completion from nvim-cmp.
-					--  Generally you don't need this, because nvim-cmp will display
-					--  completions whenever it has completion options available.
-					["<C-c>"] = cmp.mapping.complete({}),
-
-					-- Think of <c-l> as moving to the right of your snippet expansion.
-					--  So if you have a snippet that's like:
-					--  function $name($args)
-					--    $body
-					--  end
-					--
-					-- <c-l> will move you to the right of each of the expansion locations.
-					-- <c-h> is similar, except moving you backwards.
-					["<C-l>"] = cmp.mapping(function()
-						if luasnip.expand_or_locally_jumpable() then
+					["<C-Space>"] = cmp.mapping.complete({}),
+					["<CR>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							local entry = cmp.get_selected_entry()
+							if entry ~= nil then
+								if entry.source then
+									if entry.source.name ~= "nvim_lsp_signature_help" then
+										cmp.confirm({ select = true })
+									else
+										cmp.abort()
+										fallback()
+									end
+								else
+									cmp.confirm({ select = true })
+								end
+							else
+								fallback()
+							end
+						else
+							fallback()
+						end
+					end),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_locally_jumpable() then
 							luasnip.expand_or_jump()
+						else
+							fallback()
 						end
 					end, { "i", "s" }),
-					["<C-h>"] = cmp.mapping(function()
-						if luasnip.locally_jumpable(-1) then
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.locally_jumpable(-1) then
 							luasnip.jump(-1)
+						else
+							fallback()
 						end
 					end, { "i", "s" }),
-
-					-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-					--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
 				}),
 				sources = {
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
-					{ name = "path" },
 				},
 			})
 		end,
@@ -888,9 +890,7 @@ vim.keymap.set("i", "jk", "<Esc>", { noremap = true })
 vim.keymap.set("t", "jk", "<C-\\><C-n>")
 vim.keymap.set("t", "<esc>", "<C-\\><C-n>")
 
--- [[ Configure LSP ]]
--- This function gets run when an LSP connects to a particular buffer.
--- document existing key chains
+-- This is used to explain which keybindings
 require("which-key").add({
 	{ "<leader>c", group = "[C]ode" },
 	{ "<leader>c_", hidden = true },
@@ -908,73 +908,6 @@ require("which-key").add({
 	{ "<leader>w_", hidden = true },
 })
 
--- -- Setup neovim lua configuration
--- require("neodev").setup()
---
---
--- -- [[ Configure nvim-cmp ]]
--- -- See `:help cmp`
--- local cmp = require 'cmp'
--- local luasnip = require 'luasnip'
--- require('luasnip.loaders.from_vscode').lazy_load()
--- luasnip.config.setup {}
---
--- cmp.setup {
---   snippet = {
---     expand = function(args)
---       luasnip.lsp_expand(args.body)
---     end,
---   },
---   mapping = cmp.mapping.preset.insert {
---     ['<C-n>'] = cmp.mapping.select_next_item(),
---     ['<C-p>'] = cmp.mapping.select_prev_item(),
---     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
---     ['<C-f>'] = cmp.mapping.scroll_docs(4),
---     ['<C-Space>'] = cmp.mapping.complete {},
---     ['<CR>'] = cmp.mapping(
---       function(fallback)
---         if cmp.visible() then
---           local entry = cmp.get_selected_entry()
---           if entry ~= nil then
---             if entry.source then
---               if entry.source.name ~= 'nvim_lsp_signature_help' then
---                 cmp.confirm { select = true }
---               else
---                 cmp.abort()
---                 fallback()
---               end
---             else
---               cmp.confirm { select = true }
---             end
---           else
---             fallback()
---           end
---         else
---           fallback()
---         end
---       end
---     ),
---     ['<Tab>'] = cmp.mapping(function(fallback)
---       if cmp.visible() then
---         cmp.select_next_item()
---       elseif luasnip.expand_or_locally_jumpable() then
---         luasnip.expand_or_jump()
---       else
---         fallback()
---       end
---     end, { 'i', 's' }),
---     ['<S-Tab>'] = cmp.mapping(function(fallback)
---       if cmp.visible() then
---         cmp.select_prev_item()
---       elseif luasnip.locally_jumpable(-1) then
---         luasnip.jump(-1)
---       else
---         fallback()
---       end
---     end, { 'i', 's' }),
---   },
---   sources = {
---     { name = 'nvim_lsp' },
---     { name = 'luasnip' },
---   },
--- }
+-- seting up color for highlight and completion
+-- vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#393f4a", fg = "NONE" }) -- Adjust colors as needed
+-- vim.api.nvim_set_hl(0, "Pmenu", { bg = "#282c34", fg = "#abb2bf" }) -- Background and text color for menu
