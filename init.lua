1d5fa679708f12effd1ef9bee09c5feedabfe086
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-06 21:29:15 -0400

    updated telescope config

--- init.lua
+++ init.lua
@@ -37,8 +37,9 @@ require("lazy").setup(plugins, opts)
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
 
-require("telescope").setup()
+-- Telescope config
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
