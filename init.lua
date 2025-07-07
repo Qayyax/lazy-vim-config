72f72c500c4f2a6a2777ec367963be8a0c2e5cbc
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-07 13:58:44 -0400

    added neotree

--- init.lua
+++ init.lua
@@ -30,6 +30,25 @@ local plugins = {
     dependencies = {'nvim-lua/plenary.nvim'}
   }, 
   {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build=":TSUpdate"},
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- Optional image support for file preview: See `# Preview Mode` for more information.
+      -- {"3rd/image.nvim", opts = {}},
+      -- OR use snacks.nvim's image module:
+      "folke/snacks.nvim",
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    ---@module "neo-tree"
+    ---@type neotree.Config?
+    opts = {
+      -- add options here
+    },
+  }
 }
 
 require("lazy").setup(plugins, opts)
@@ -71,3 +90,20 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help ta
     highlight = { enable = true },
     indent = { enable = true }
   })
+
+-- Neotree
+local neotree = require("neo-tree")
+neotree.setup({
+  event_handlers = {
+    {
+      event = "file_opened",
+      handler = function(file_path)
+        -- auto close
+        -- vimc.cmd("Neotree close")
+        -- OR
+        require("neo-tree.command").execute({ action = "close" })
+      end
+    },
+  }
+})
+vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>')
