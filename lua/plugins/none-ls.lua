2d6b2dbae607d7fab6a8d7fa7cdeee61d0f306a4
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-08 19:50:15 -0400

    added wiki for builtins in none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,37 +1,40 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {'nvim-lua/plenary.nvim'},
-  config = function()
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.markdownlint,
-      },
+		null_ls.setup({
+			-- Docs: (for the builtin formatters)
+			-- https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.markdownlint,
+			},
 
-      -- autoformat on save
-      -- wiki: https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({
-                async = false,
-                -- c for client, to make sure that we don't have formatting conflict with lsp
-                filter = function(c) return c.name == "null-ls" end
-              })
-            end,
-          })
-        end
-      end,
-    })
-
-  end
+			-- autoformat on save
+			-- wiki: https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({
+								async = false,
+								-- c for client, to make sure that we don't have formatting conflict with lsp
+								filter = function(c)
+									return c.name == "null-ls"
+								end,
+							})
+						end,
+					})
+				end
+			end,
+		})
+	end,
 }
