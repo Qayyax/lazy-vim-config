3016021f2fdc8b3ae11fda5e62dc34f8d26d5542
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-08 12:01:10 -0400

    keybinds for vim.lspconfig

--- lua/plugins/lsp-configuration.lua
+++ lua/plugins/lsp-configuration.lua
@@ -12,9 +12,23 @@ return {
     opts = {
       auto_install = true,
     }
-  }, 
+  },
   {
     "neovim/nvim-lspconfig",
     lazy = false,
+    config = function()
+      -- local lspconfig = require("lspconfig")
+      -- lspconfig.lua_ls.setup({})
+      -- vim.lsp.config("lua_ls", {})
+      vim.lsp.enable("lua_ls")
+
+      -- Keybinds
+      -- docs here https://neovim.io/doc/user/lsp.html#vim.lsp.buf.definition()
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+    end
   }
 }
