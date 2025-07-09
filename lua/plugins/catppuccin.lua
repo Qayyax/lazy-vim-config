0aa978b7849926dd863dac748d4c588865ff860c
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-09 12:18:01 -0400

    removed white space

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,25 +1,24 @@
 -- https://github.com/catppuccin/nvim?tab=readme-ov-file
 
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  opts = { },
-  config = function()
-    require("catppuccin").setup({
-      flavour = "auto",
-      background = {
-        dark = "mocha",
-      },
-      no_italic = false,
-      no_bold = false,
-      transparent_background = true, 
-      styles = {
-        comments = {"italic"},
-        conditionals = {"italic"},
-      },
-    })
-    vim.cmd.colorscheme "catppuccin"
-  end
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	opts = {},
+	config = function()
+		require("catppuccin").setup({
+			flavour = "auto",
+			background = {
+				dark = "mocha",
+			},
+			no_italic = false,
+			no_bold = false,
+			transparent_background = true,
+			styles = {
+				comments = { "italic" },
+				conditionals = { "italic" },
+			},
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
-
