4cea713f6c35982a63bb2b4772d7d16b67d1acb2
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-08 23:50:07 -0400

    added isort for python

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -11,6 +11,7 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.diagnostics.markdownlint,
 			},
