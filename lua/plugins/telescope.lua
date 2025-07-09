3bb2003b666faaa60b038d4c5f599bbe56c6ea99
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-09 13:33:47 -0400

    added show dianostics

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,17 +9,18 @@ return {
 		config = function()
 			-- Telescope config
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-			vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "Telescope buffers" })
-			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-			vim.keymap.set("n", "<leader>fq", builtin.quickfix, { desc = "Telescope quickfix list" })
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep through all files" })
+			vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "Show Buffers" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = " [F]ind [H]elp tags" })
+			vim.keymap.set("n", "<leader>fq", builtin.quickfix, { desc = " [F]ind [Q]uickfix" })
 			vim.keymap.set(
 				"n",
 				"<leader>/",
 				builtin.current_buffer_fuzzy_find,
 				{ desc = "[/] Fuzzily search in current buffer" }
 			)
+			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "Search Diagnostics" })
 		end,
 	},
 	{
