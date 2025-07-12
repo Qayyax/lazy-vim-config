5f6cf5530be43279763a4357c1d5560a40635fb6
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-12 18:05:14 -0400

    render image at cursor

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -2,8 +2,8 @@ return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
 	---@module 'render-markdown'
-	-- -@type render.md.UserConfig
 	opts = {
 		completions = { lsp = { enabled = true } },
+		only_render_image_at_cursor = true,
 	},
 }
