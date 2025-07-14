9f7811a898beb2ec6f668b42a81540ce13ff58ff
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-14 13:33:52 -0400

    key binds

---
+++ lua/plugins/snacks/key.lua
@@ -0,0 +1,259 @@
+return {
+	-- Top Pickers & Explorer
+	{
+		"<leader><space>",
+		function()
+			Snacks.picker.buffers()
+		end,
+		desc = "Buffers",
+	},
+	{
+		"<leader>n",
+		function()
+			Snacks.explorer()
+		end,
+		desc = "File Explorer",
+	},
+
+	-- find
+	{
+		"<leader>fc",
+		function()
+			Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+		end,
+		desc = "Find Config File",
+	},
+	{
+		"<leader>ff",
+		function()
+			Snacks.picker.files()
+		end,
+		desc = "Find Files",
+	},
+	{
+		"<leader>fp",
+		function()
+			Snacks.picker.projects()
+		end,
+		desc = "Projects",
+	},
+	{
+		"<leader>fr",
+		function()
+			Snacks.picker.recent()
+		end,
+		desc = "Recent",
+	},
+
+	-- git
+	{
+		"<leader>gb",
+		function()
+			Snacks.picker.git_branches()
+		end,
+		desc = "Git Branches",
+	},
+	{
+		"<leader>gf",
+		function()
+			Snacks.picker.git_files()
+		end,
+		desc = "Find Git Files",
+	},
+	{
+		"<leader>gl",
+		function()
+			Snacks.picker.git_log()
+		end,
+		desc = "Git Log",
+	},
+	{
+		"<leader>gs",
+		function()
+			Snacks.picker.git_status()
+		end,
+		desc = "Git Status",
+	},
+	{
+		"<leader>gS",
+		function()
+			Snacks.picker.git_stash()
+		end,
+		desc = "Git Stash",
+	},
+	{
+		"<leader>gd",
+		function()
+			Snacks.picker.git_diff()
+		end,
+		desc = "Git Diff (Hunks)",
+	},
+
+	-- Grep
+	{
+		"<leader>/",
+		function()
+			Snacks.picker.grep()
+		end,
+		desc = "Grep",
+	},
+	{
+		"<leader>/b",
+		function()
+			Snacks.picker.lines()
+		end,
+		desc = "Buffer Lines",
+	},
+	{
+		"<leader>//",
+		function()
+			Snacks.picker.grep_buffers()
+		end,
+		desc = "Grep Open Buffers",
+	},
+	{
+		"<leader>/w",
+		function()
+			Snacks.picker.grep_word()
+		end,
+		desc = "Visual selection or word",
+		mode = { "n", "x" },
+	},
+
+	-- search
+	{
+		'<leader>s"',
+		function()
+			Snacks.picker.registers()
+		end,
+		desc = "Registers",
+	},
+	{
+		"<leader>sc",
+		function()
+			Snacks.picker.commands()
+		end,
+		desc = "Commands",
+	},
+	{
+		"<leader>q",
+		function()
+			Snacks.picker.diagnostics()
+		end,
+		desc = "Diagnostics",
+	},
+	{
+		"<leader>e",
+		function()
+			Snacks.picker.diagnostics_buffer()
+		end,
+		desc = "Buffer Diagnostics",
+	},
+	{
+		"<leader>sh",
+		function()
+			Snacks.picker.help()
+		end,
+		desc = "Help Pages",
+	},
+	{
+		"<leader>si",
+		function()
+			Snacks.picker.icons()
+		end,
+		desc = "Icons",
+	},
+	{
+		"<leader>sk",
+		function()
+			Snacks.picker.keymaps()
+		end,
+		desc = "Keymaps",
+	},
+	{
+		"<leader>sl",
+		function()
+			Snacks.picker.loclist()
+		end,
+		desc = "Location List",
+	},
+	{
+		"<leader>sm",
+		function()
+			Snacks.picker.man()
+		end,
+		desc = "Man Pages",
+	},
+	{
+		"<leader>sp",
+		function()
+			Snacks.picker.lazy()
+		end,
+		desc = "Search for Plugin Spec",
+	},
+	{
+		"<leader>sq",
+		function()
+			Snacks.picker.qflist()
+		end,
+		desc = "Quickfix List",
+	},
+	{
+		"<leader>sR",
+		function()
+			Snacks.picker.resume()
+		end,
+		desc = "Resume",
+	},
+	{
+		"<leader>su",
+		function()
+			Snacks.picker.undo()
+		end,
+		desc = "Undo History",
+	},
+	{
+		"<leader>uC",
+		function()
+			Snacks.picker.colorschemes()
+		end,
+		desc = "Colorschemes",
+	},
+	-- LSP
+	{
+		"gd",
+		function()
+			Snacks.picker.lsp_definitions()
+		end,
+		desc = "Goto Definition",
+	},
+	{
+		"gD",
+		function()
+			Snacks.picker.lsp_declarations()
+		end,
+		desc = "Goto Declaration",
+	},
+	{
+		"gr",
+		function()
+			Snacks.picker.lsp_references()
+		end,
+		nowait = true,
+		desc = "References",
+	},
+	{
+		"gi",
+		function()
+			Snacks.picker.lsp_implementations()
+		end,
+		desc = "Goto Implementation",
+	},
+	{
+		"gt",
+		function()
+			Snacks.picker.lsp_type_definitions()
+		end,
+		desc = "Goto [t]ype Definition",
+	},
+}
