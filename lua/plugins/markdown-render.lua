8f5ea717f6b6c9c9b3b94052c8ca54650572796d
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-24 00:31:01 -0400

    config for markdown file

--- lua/plugins/markdown-render.lua
+++ lua/plugins/markdown-render.lua
@@ -3,5 +3,32 @@ return {
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 	opts = {
 		completions = { lsp = { enabled = true } },
+		heading = {
+			backgrounds = { "RenderMarkdownHdNoBg" },
+			position = "inline",
+		},
+		code = {
+			backgrounds = { "RenderMarkdownHdNoBg" },
+			position = "left",
+			style = "language",
+			border = "hide",
+			above = "",
+			below = "",
+			conceal_delimeters = true,
+		},
+		callout = {
+			note = { highlight = "RenderMarkdownHdNoBg" },
+			tip = { highlight = "RenderMarkdownHdNoBg" },
+			important = { highlight = "RenderMarkdownHdNoBg" },
+			warning = { highlight = "RenderMarkdownHdNoBg" },
+			danger = { highlight = "RenderMarkdownHdNoBg" },
+		},
+		quote = {
+			highlight = "RenderMarkdownHdNoBg",
+		},
+		-- Turn off sign backgrounds if you want
+		-- sign = {
+		-- 	enabled = false,
+		-- },
 	},
 }
