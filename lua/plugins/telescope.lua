57e57265cf173166edef6dab1767c810ab50c6f6
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-08-13 11:24:12 -0400

    added telescope, todo depended on it

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,10 @@
+-- https://github.com/nvim-telescope/telescope.nvim?tab=readme-ov-file#installation
+--
+return {
+	{
+		"nvim-telescope/telescope.nvim",
+		name = "telescope",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
+	},
+}
