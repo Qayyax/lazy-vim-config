f55f5827c408172b13e8ca61f65a844fad1f9099
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-10-01 17:16:19 -0400

    removed diagnostics, it shows on lualine_b

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,11 +6,9 @@ return {
 		require("lualine").setup({
 			sections = {
 				lualine_x = { "lsp_status", "filetype" },
-				lualine_y = { "diagnostics" },
 			},
 			inactive_sections = {
 				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_y = { "diagnostics" },
 			},
 		})
 	end,
