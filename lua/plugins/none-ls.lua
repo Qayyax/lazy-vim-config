008bab7148aacb4acaa8eb1f8613be2abe43ca62
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-11-14 19:48:39 -0500

    commented out swiftlint for now

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -17,7 +17,7 @@ return {
 				null_ls.builtins.formatting.swiftformat.with({
 					extra_args = { "--commas", "never" },
 				}),
-				null_ls.builtins.diagnostics.swiftlint,
+				-- null_ls.builtins.diagnostics.swiftlint,
 			},
 
 			-- autoformat on save
